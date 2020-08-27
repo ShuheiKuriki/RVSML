72c3e29d95dc23a230c7f8685d1c758f691a0e80
shuhei <shukuri.7336.8@gmail.com>
2020-08-27 21:41:52 +0900

    linting対応

--- Python/Evaluate_MSRAction3D_60.py
+++ Python/Evaluate_MSRAction3D_60.py
@@ -1,58 +1,82 @@
+"""
+MSRAction3Dの学習・分類を実行
+"""
+import logging, pickle, time, os, argparse, torch
 import numpy as np
 from rvsml_torch.run_RVSML import run_RVSML
-import logging,pickle,time,os,argparse,torch
-np.set_printoptions(precision=3,suppress=True)
+from src.utils import bool_flag
+np.set_printoptions(precision=3, suppress=True)
 
 if 'args':
     parser = argparse.ArgumentParser(description='MSRAction3D')
+    parser.add_argument("--cuda", type=bool_flag, default=True, help="Run on GPU")
     # highpara
     parser.add_argument("--method", type=str, default='dtw', help="alignment method")
     parser.add_argument("--v_length", type=int, default=4, help="the rate of the templatenum")
-    # parser.add_argument("--lambda0", type=float, default=0.1, help="the parameter of the rotation matrix")
-    parser.add_argument("--lambda1", type=float, default=0.001, help="the parameter of the inverse difference moment")
+    parser.add_argument("--lambda0_for_dtw", type=float, default=0.1, help="the parameter of the rotation matrix")
+    parser.add_argument("--lambda0_for_other", type=float, default=0.0001, help="the parameter of the rotation matrix")
+    parser.add_argument("--lambda1", type=float, default=0.01, help="the parameter of the inverse difference moment")
     parser.add_argument("--lambda2", type=float, default=0.01, help="the parameter of the standard distribution")
-    parser.add_argument("--delta", type=float, default=5,help="variance of the standard distribution")
+    parser.add_argument("--delta", type=float, default=3, help="variance of the standard distribution")
     parser.add_argument("--init_delta", type=float, default=1, help="variance of the standard distribution")
     parser.add_argument("--reg", type=float, default=0.003, help="regularization parameter of sinkhorn distance")
     parser.add_argument("--init", type=str, default='normal', help="initial by random")
-    parser.add_argument("--distance", type=str, default='sqeuclidean', help="type of distnce")
+    parser.add_argument("--metric", type=str, default='sqeuclidean', help="type of distnce")
+    parser.add_argument("--solve_method", type=str, default='Adagrad', help="type of distnce")
+    parser.add_argument("--lr", type=float, default=0.001, help="lerning rate of gradient descent")
+    parser.add_argument("--alpha", type=float, default=0.001, help="map_beta")
 
 params = parser.parse_args()
 class Options:
+    """
+    オプションを定義
+    """
     def __init__(self):
-        self.max_iters, self.err_limit = 100, 10**(-4)
+        self.max_epoch, self.err_limit = 10000, 10**(-5)
         self.method = params.method
         self.init = params.init
         self.init_delta = params.init_delta
         self.classify = 'knn'
         if self.method == 'opw':
             self.lambda1, self.lambda2, self.delta = params.lambda1, params.lambda2, params.delta
-        self.lambda0 = 0.1
-        self.distance = params.distance
+        if self.method == 'dtw':
+            self.lambda0 = params.lambda0_for_dtw
+        else:
+            self.lambda0 = params.lambda0_for_other
+        self.metric = params.metric
         self.regularize = params.reg
+        self.cuda = params.cuda
+        self.dtype = torch.float64
+        self.solve_method = params.solve_method
+        self.lr = params.lr
+        self.alpha = params.alpha
 
 class Dataset:
+    """
+    データセットを用意
+    """
     def __init__(self):
         self.dataname = 'MSRAction3D'
         self.classnum, self.dim = 20, 60
-        self.trainsetdatanum, self.testsetdatanum  = 284, 273
+        self.trainsetdatanum, self.testsetdatanum = 284, 273
         self.ClassLabel = torch.arange(self.classnum).t()+1
         self.trainsetdatalabel, self.testsetdatalabel = [0], [0]
         self.templatenums = [params.v_length]*self.classnum
 
     def getlabelfull(self):
-        self.trainsetlabelfull = self.getLabel(self.trainsetdatalabel)
-        self.testsetlabelfull = self.getLabel(self.testsetdatalabel)
-        return
+        """今使ってない"""
+        self.trainsetlabelfull = getLabel(self.trainsetdatalabel)
+        self.testsetlabelfull = getLabel(self.testsetdatalabel)
 
-    def getLabel(self,classid):
-        p = int(max(classid))
-        # logger.info(p)
-        X = np.zeros((len(classid),p))-1
-        for i in range(p):
-            indx = np.nonzero(classid == i+1)
-            X[indx,i] = 1
-        return X
+def getLabel(classid):
+    """これも使ってない"""
+    p = int(max(classid))
+    # logger.info(p)
+    X = np.zeros((len(classid), p))-1
+    for q in range(p):
+        indx = np.nonzero(classid == q+1)
+        X[indx, q] = 1
+    return X
 
 options = Options()
 dataset = Dataset()
@@ -65,33 +89,28 @@ if 'logger':
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
     if params.method == 'opw':
-        filename='MSRAction3D/log/{}/v{}_l2-{}_del{}_init-{}_init-del{}_l1-{}.log'.format(params.method,params.v_length,params.lambda2,params.delta,params.init,params.init_delta,params.lambda1)
-    elif params.method in ['dtw','greedy','OT']:
-        filename = 'MSRAction3D/log/{}/v{}_init-{}_init-del{}.log'.format(params.method,params.v_length,params.init,params.init_delta)
+        filename = 'MSRAction3D/log/{}/v{}_l2-{}_del{}_init-{}_init-del{}_l1-{}.log'.format(params.method, params.v_length, params.lambda2, params.delta, params.init, params.init_delta, params.lambda1)
+    elif params.method in ['dtw', 'greedy', 'OT']:
+        filename = 'MSRAction3D/log/{}/v{}_init-{}_init-del{}.log'.format(params.method, params.v_length, params.init, params.init_delta)
     elif params.method == 'sinkhorn':
-        filename='MSRAction3D/log/{}/v{}_reg{}_init-{}_init-del{}.log'.format(params.method,params.v_length,params.reg,params.init,params.init_delta)
+        filename = 'MSRAction3D/log/{}/v{}_reg{}_init-{}_init-del{}.log'.format(params.method, params.v_length, params.reg, params.init, params.init_delta)
     logging.basicConfig(filename=filename, format="%(message)s", filemode='w') # ログのファイル出力先を設定
 
-pickle_list = ['trainsetnum','trainsetdatalabel','testsetdatalabel','trainsetdata','testsetdata','trainset']
+pickle_list = ['trainsetnum', 'trainsetdatalabel', 'testsetdatalabel', 'trainsetdata', 'testsetdata', 'trainset']
 
 for v in pickle_list:
-    exec("with open('MSRAction3D/data/{}.bin','rb') as f: dataset.{} = pickle.load(f)".format(v,v))
+    exec("with open('MSRAction3D/data/%s.bin','rb') as f: dataset.%s = pickle.load(f)" %(v, v))
 
+device = 'cuda' if params.cuda else 'cpu'
 for i in range(len(dataset.trainsetdata)):
-    dataset.trainsetdata[i] = torch.from_numpy(dataset.trainsetdata[i].astype(np.float32)).clone()
+    dataset.trainsetdata[i] = torch.from_numpy(dataset.trainsetdata[i]).clone().to(device)
 for i in range(len(dataset.testsetdata)):
-    dataset.testsetdata[i] = torch.from_numpy(dataset.testsetdata[i].astype(np.float32)).clone()
+    dataset.testsetdata[i] = torch.from_numpy(dataset.testsetdata[i]).clone().to(device)
 for i in range(len(dataset.trainset)):
     for j in range(len(dataset.trainset[i])):
-        dataset.trainset[i][j] = torch.from_numpy(dataset.trainset[i][j].astype(np.float32)).clone()
+        dataset.trainset[i][j] = torch.from_numpy(dataset.trainset[i][j]).clone().to(device)
 
 dataset.getlabelfull()
 
-accs = []
-for l0 in [0.001,0.01,0.1]:
-    options.lambda0 = l0
-    dataset,knn_acc,virtual_acc = run_RVSML(dataset,options)
-    accs.append([knn_acc,virtual_acc])
-
-for i,l0 in enumerate([0.001,0.01,0.1]):
-    logger.info('lambda0:{}, knn_acc:{}, virtual_acc:{}'.format(l0,accs[i][0],accs[i][1]))
+dataset, knn_acc, virtual_acc = run_RVSML(dataset, options)
+logger.info("method: %s, knn_acc: %.4f, virtual_acc: %.4f", params.method, knn_acc, virtual_acc)
