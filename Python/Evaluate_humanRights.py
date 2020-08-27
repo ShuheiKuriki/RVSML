f309fd4d8a2ecebc064e3797d7ddacd6cd544fca
shuhei <shukuri.7336.8@gmail.com>
2020-08-27 21:42:30 +0900

    勾配降下実装

--- Python/Evaluate_humanRights.py
+++ Python/Evaluate_humanRights.py
@@ -1,10 +1,11 @@
+"""CWLE learning with humanRights"""
+import logging, pickle, time, os, argparse, torch
 import numpy as np
-from rvsml.run_RVSML import run_RVSML
+from rvsml_torch.run_RVSML import run_RVSML
 from set_text import read_txt_embeddings
-import logging,pickle,time,os,argparse,torch
 from src.utils import bool_flag
 from src.evaluation.word_translation import get_word_translation_accuracy
-np.set_printoptions(precision=3,suppress=True, threshold=10000)
+np.set_printoptions(precision=3, suppress=True, threshold=10000)
 
 if 'args':
     # main
@@ -14,7 +15,7 @@ if 'args':
     parser.add_argument("--exp_path", type=str, default="", help="Where to store experiment logs and models")
     parser.add_argument("--exp_name", type=str, default="debug", help="Experiment name")
     parser.add_argument("--exp_id", type=str, default="", help="Experiment ID")
-    parser.add_argument("--cuda", type=bool_flag, default=True, help="Run on GPU")
+    parser.add_argument("--cuda", type=bool_flag, default=False, help="Run on GPU")
     parser.add_argument("--export", type=str, default="txt", help="Export embeddings after training (txt / pth)")
     # data
     # parser.add_argument("--langnum", type=int, default=2, help="the number of languages")
@@ -22,14 +23,16 @@ if 'args':
     parser.add_argument("--w2v_dim", type=int, default=300, help="the dimension of word2vec")
     # highpara
     parser.add_argument("--method", type=str, default='dtw', help="alignment method")
-    parser.add_argument("--v_rate", type=int, default=7, help="the rate of the templatenum")
+    parser.add_argument("--v_rate", type=int, default=8, help="the rate of the templatenum")
     parser.add_argument("--lambda1", type=float, default=0.001, help="the parameter of the inverse difference moment")
     parser.add_argument("--lambda2", type=float, default=0.01, help="the parameter of the standard distribution")
     parser.add_argument("--delta", type=float, default=1, help="variance of the standard distribution")
     parser.add_argument("--init_delta", type=float, default=1, help="variance of the standard distribution")
-    parser.add_argument("--reg", type=float, default=0.003, help="regularization parameter of sinkhorn distance")
+    parser.add_argument("--reg", type=float, default=0.003, help="regularization parameter of sinkhorn metric")
     parser.add_argument("--init", type=str, default='normal', help="initial by random")
-    parser.add_argument("--distance", type=str, default='cosine', help="type of distnce")
+    parser.add_argument("--metric", type=str, default='cosine', help="type of distnce")
+    parser.add_argument("--solve_method", type=str, default='analytic', help="how to optimize")
+    parser.add_argument("--alpha", type=float, default=0.001, help="map_beta")
 
     if "mapping":
         parser.add_argument("--map_id_init", type=bool_flag, default=True, help="Initialize the mapping as an identity matrix")
@@ -83,8 +86,9 @@ if 'args':
         assert params.export in ["", "txt", "pth"]
 
 class Options:
+    """学習のオプション"""
     def __init__(self):
-        self.max_iters, self.err_limit = 1000, 10**(-4)
+        self.max_epoch, self.err_limit = 1000, 10**(-4)
         if params.method == 'dtw':
             self.lambda0 = 0.1
         elif params.method == 'greedy':
@@ -101,45 +105,48 @@ class Options:
         self.init = params.init
         self.init_delta = params.init_delta
         # self.templatenums = 1
-        self.distance = params.distance
+        self.metric = params.metric
         self.regularize = params.reg
+        self.cuda = params.cuda
+        self.alpha = params.alpha
+        self.solve_method = params.solve_method
 
 class Dataset:
-    def __init__(self,data=None):
+    """データセット関連"""
+    def __init__(self, data=None):
         self.dataname = 'humanRights'
-        self.langs = ['en','es']
+        self.langs = ['en', 'es']
         self.langnum, self.classnum, self.dim = len(self.langs), params.classnum, params.w2v_dim
         self.trainsetdatanum = self.langnum * self.classnum
         self.trainsetnum = [self.langnum] * self.classnum
         self.testsetdatanum = self.trainsetdatanum
-        self.ClassLabel = np.arange(self.classnum).T+1
+        self.ClassLabel = torch.arange(self.classnum).T+1
         self.trainsetdatalabel = [1+ i//self.langnum for i in range(self.trainsetdatanum)]
         self.testsetdatalabel = self.trainsetdatalabel
         self.L = 0
         if data is not None:
-            self.trainsetdata,self.testsetdata = data,data
+            self.trainsetdata, self.testsetdata = data, data
             trainset = [[0]*self.langnum for _ in range(self.classnum)]
-            self.templatenums = np.zeros(self.classnum,dtype=int)
+            self.templatenums = torch.zeros(self.classnum, dtype=torch.int32)
             for c in range(self.classnum):
                 for l in range(self.langnum):
                     trainset[c][l] = data[c*self.langnum+l]
                     self.templatenums[c] += len(trainset[c][l])
-                self.templatenums[c] //= self.langnum*params.v_rate
+                self.templatenums[c] = max(self.templatenums[c]//(self.langnum*params.v_rate), 1)
             self.trainset = trainset
 
-    def getlabelfull(self):
-        self.trainsetlabelfull = self.getLabel(self.trainsetdatalabel)
-        self.testsetlabelfull = self.getLabel(self.testsetdatalabel)
-        return
+    # def getlabelfull(self):
+    #     self.trainsetlabelfull = self.getLabel(self.trainsetdatalabel)
+    #     self.testsetlabelfull = self.getLabel(self.testsetdatalabel)
 
-    def getLabel(self,classid):
-        p = int(max(classid))
-        # logger.info(p)
-        X = np.zeros((np.size(classid),p))-1
-        for i in range(p):
-            indx = np.nonzero(classid == i+1)
-            X[indx,i] = 1
-        return X
+    # def getLabel(self, classid):
+    #     p = int(max(classid))
+    #     # logger.info(p)
+    #     X = torch.zeros((np.size(classid), p))-1
+    #     for i in range(p):
+    #         indx = np.nonzero(classid == i+1)
+    #         X[indx, i] = 1
+    #     return X
 
 options = Options()
 dataset = Dataset()
@@ -150,30 +157,32 @@ logger.addHandler(logging.StreamHandler()) # ログのコンソール出力の�
 lang_chr = ''
 for i in range(dataset.langnum):
     lang_chr += dataset.langs[i]
-dirname = '{}/log/{}_{}/'.format(dataset.dataname,lang_chr,params.method)
+dirname = '{}/log/{}_{}/'.format(dataset.dataname, lang_chr, params.method)
 if not os.path.isdir(dirname):
     os.mkdir(dirname)
-logging.basicConfig(filename='{}/log/{}_{}/init-{}{}.log'.format(dataset.dataname,lang_chr,params.method,params.init,params.init_delta), format="%(message)s", filemode='w') # ログのファイル出力先を設定
+logging.basicConfig(filename='{}/log/{}_{}/init-{}{}.log'.format(dataset.dataname, lang_chr, params.method, params.init, params.init_delta), format="%(message)s", filemode='w') # ログのファイル出力先を設定
 
-data = []
-for c in range(dataset.classnum):
-    for l in dataset.langs:
-        data.append(np.load('{}/vectorized_texts/{}/sentence{}.npy'.format(dataset.dataname,l,c+1)))
-dataset = Dataset(data)
+device = 'cuda' if params.cuda else 'cpu'
+print(device)
+data1 = []
+for cla in range(dataset.classnum):
+    for lang in dataset.langs:
+        d = np.load('{}/vectorized_texts/{}/sentence{}.npy'.format(dataset.dataname, lang, cla+1))
+        data1.append(torch.from_numpy(d).clone().to(device))
+dataset = Dataset(data1)
 dataset.getlabelfull()
 
-dataset,knn_accs,virtual_acc = run_RVSML(dataset,options)
+dataset, knn_accs, virtual_acc = run_RVSML(dataset, options)
 
 dicos = [0]*dataset.langnum
 embs = [0]*dataset.langnum
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 for i in range(dataset.langnum):
-    dicos[i],embs[i] = read_txt_embeddings(dataset.langs[i])
-    embs[i] = torch.from_numpy(np.dot(embs[i],dataset.L).astype(np.float32)).clone().to(device)
+    dicos[i], embs[i] = read_txt_embeddings(dataset.langs[i])
+    embs[i] = torch.from_numpy(np.dot(embs[i], dataset.L).astype(np.float32)).clone().to(device)
 for i in range(dataset.langnum):
     for j in range(dataset.langnum):
-        if i==j:
+        if i == j:
             continue
-        results = get_word_translation_accuracy(dataset.langs[i], dicos[i].word2id, embs[i], dataset.langs[j], dicos[j].word2id, embs[j], "nn", 'default')
-        logger.info('{}-{}: {}'.format(dataset.langs[i],dataset.langs[j],results))
-# logger.info(accs)
+        results = get_word_translation_accuracy(dataset.langs[i], dicos[i].word2id, embs[i], dataset.langs[j], dicos[j].word2id, embs[j], "csls_knn_10", 'default')
+        logger.info('%s-%s: %.5f', dataset.langs[i], dataset.langs[j], results)
+# logger.info(acc
--- Python/rvsml_torch/RVSML_OT_Learning.py
+++ Python/rvsml_torch/RVSML_OT_Learning.py
@@ -1,7 +1,28 @@
+"""torchを用いた学習パート"""
+import math, torch, logging
+from numpy import linalg
+from torch.utils.data import DataLoader
+import torch.optim as optim
 from .align import get_alignment
-import logging,torch,math
 
-def RVSML_OT_Learning(dataset,options):
+class Dataset2:
+    """確率的勾配降下用のデータセット"""
+    def __init__(self, tsd, Vs, Ts, L, device):
+        self.dist = torch.zeros(len(tsd), dtype=torch.float64, device=device)
+        for i, (ts, V, T) in enumerate(zip(tsd, Vs, Ts)):
+            for j, tsj in enumerate(ts):
+                for k, vk in enumerate(V):
+                    self.dist[i] += T[j, k] * torch.norm(torch.mv(L.t(), tsj) - vk)**2
+
+    def __len__(self):
+        return len(self.dist)
+
+    def __getitem__(self, index):
+        return self.dist[index]
+
+def RVSML_OT_Learning(dataset, options):
+    """torchを用いた学習"""
+    device = 'cuda' if options.cuda else 'cpu'
     logger = logging.getLogger('{}Log'.format(dataset.dataname))
 
     classnum = dataset.classnum
@@ -9,93 +30,151 @@ def RVSML_OT_Learning(dataset,options):
     downdim = sum(templatenums)
     trainset = dataset.trainset
     dim = dataset.dim
-    trainsetnum = dataset.trainsetnum
+    tn = dataset.trainsetnum
+    tdn = dataset.trainsetdatanum
+    beta = options.lambda0
+    alpha = options.alpha
 
     virtual_sequence = [0]*classnum
     active_dim = -1
     for c in range(classnum):
-        virtual_sequence[c] = torch.zeros(templatenums[c],downdim)
+        virtual_sequence[c] = torch.zeros((templatenums[c], downdim), dtype=torch.float64).to(device)
         for a_d in range(templatenums[c]):
             active_dim += 1
-            virtual_sequence[c][a_d,active_dim] = 1
+            virtual_sequence[c][a_d, active_dim] = 1
 
     ## inilization
-    R_A = torch.zeros(dim,dim)
-    R_B = torch.zeros(dim,downdim)
-    N = sum(trainsetnum)
+    R_A = torch.zeros((dim, dim), dtype=torch.float64).to(device)
+    R_B = torch.zeros((dim, downdim), dtype=torch.float64).to(device)
     for c in range(classnum):
-        for n in range(trainsetnum[c]):
+        for n in range(tn[c]):
             seqlen = trainset[c][n].size()[0]
             # test = trainset[c][0][n]
             if options.init == 'uniform':
-                T_ini = torch.ones((seqlen,templatenums[c]))/(seqlen*templatenums[c])
+                T_ini = torch.ones((seqlen, templatenums[c]), dtype=torch.float64).to(device)/(seqlen*templatenums[c])
             elif options.init == 'random':
-                T_ini = torch.zeros(seqlen,templatenums[c])
+                T_ini = torch.zeros((seqlen, templatenums[c]), dtype=torch.float64).to(device)
                 for i in range(seqlen):
                     for j in range(templatenums[c]):
-                        T_ini[i,j] = (1+torch.randn()*options.init_delta)/(seqlen*templatenums[c])
+                        T_ini[i, j] = (1+torch.randn()*options.init_delta)/(seqlen*templatenums[c])
             elif options.init == 'normal':
-                T_ini = torch.zeros(seqlen,templatenums[c])
+                T_ini = torch.zeros((seqlen, templatenums[c]), dtype=torch.float64).to(device)
                 mid_para = math.sqrt(1/seqlen**2 + 1/templatenums[c]**2)
                 for i in range(seqlen):
                     for j in range(templatenums[c]):
                         d = abs(i/seqlen - j/templatenums[c])/mid_para
-                        T_ini[i,j] = math.exp(-d**2/(2*options.init_delta**2))/(options.init_delta*math.sqrt(2*math.pi))
-            # print(T_ini)
+                        T_ini[i, j] = math.exp(-d**2/(2*options.init_delta**2))/(options.init_delta*math.sqrt(2*math.pi))
+            print(T_ini)
             for i in range(seqlen):
-                a = trainset[c][n][i,:]
-                temp_ra = torch.mm(a.view(len(a),1), a.view(1,len(a)))
+                a = trainset[c][n][i, :]
+                temp_ra = torch.mm(a.view(len(a), 1), a.view(1, len(a)))
                 for j in range(templatenums[c]):
-                    R_A += T_ini[i,j]*temp_ra
+                    R_A += T_ini[i, j]*temp_ra
                     # logger.info(R_A.shape,R_B.shape,temp_ra.shape)
-                    b = virtual_sequence[c][j,:]
-                    R_B += T_ini[i,j]*torch.mm(a.view((len(a),1)), b.view((1, len(b))))
+                    b = virtual_sequence[c][j, :]
+                    R_B += T_ini[i, j]*torch.mm(a.view((len(a), 1)), b.view((1, len(b))))
 
-    R_I = R_A + options.lambda0 * N * torch.eye(dim)
-    #L = inv(R_I) * R_B
-    L, _ = torch.solve(R_B,R_I)
-
+    R_I = R_A + beta * tdn * torch.eye(dim, dtype=torch.float64, device=device)
+    # L = inv(R_I) * R_B
+    R_I = R_I.cpu().numpy()
+    R_B = R_B.cpu().numpy()
+    L = linalg.solve(R_I, R_B)
+    L = torch.from_numpy(L).to(device)
+    L += alpha * (L - L.mm(L.transpose(0, 1).mm(L)))
     print("initialization done")
     print("update start")
-    ## update
+    # update
     loss_old = 10**8
-    for nIter in range(options.max_iters):
-        loss = 0
-        R_A = torch.zeros(dim,dim)
-        R_B = torch.zeros(dim,downdim)
-        # Ts = [[0]*trainsetnum[c] for _ in range(classnum)]
-        for c in range(classnum):
-            for n in range(trainsetnum[c]):
-                # logger.info(trainset[c][n])
-                seqlen = trainset[c][n].size()[0]
-                dist, T = get_alignment(torch.mm(trainset[c][n],L), virtual_sequence[c],options)
-                loss += dist
-                # Ts[c][n] = T
-                for i in range(seqlen):
-                    a = trainset[c][n][i,:]
-                    temp_ra = torch.mm(a.view((len(a),1)), a.view((1,len(a))))
-                    for j in range(templatenums[c]):
-                        b = virtual_sequence[c][j,:]
-                        R_A += T[i,j] * temp_ra
-                        R_B += T[i,j] * torch.mm(a.view((len(a),1)), b.view((1, len(b))))
-        # logger.info(Ts)
-        loss = loss/N + torch.trace(torch.mm(L.t(),L))
-        logger.info("iteration:{}, loss:{}".format(nIter,loss/N))
-        if abs(loss - loss_old) < options.err_limit:
-            logger.info(T)
-            break
-        else:
+    if options.solve_method in ['analytic', 'gradient']:
+        for epoch in range(options.max_epoch):
+            loss = 0
+            R_A = torch.zeros((dim, dim), dtype=torch.float64, device=device)
+            R_B = torch.zeros((dim, downdim), dtype=torch.float64, device=device)
+            # Ts = [[0]*tn[c] for _ in range(classnum)]
+            for c in range(classnum):
+                for n in range(tn[c]):
+                    # logger.info(trainset[c][n])
+                    seqlen = trainset[c][n].size()[0]
+                    dist, T = get_alignment(torch.mm(trainset[c][n], L), virtual_sequence[c], options)
+                    loss += dist
+                    # Ts[c][n] = T
+                    for i in range(seqlen):
+                        a = trainset[c][n][i, :]
+                        temp_ra = torch.mm(a.view((len(a), 1)), a.view((1, len(a))))
+                        for j in range(templatenums[c]):
+                            b = virtual_sequence[c][j, :]
+                            R_A += T[i, j] * temp_ra
+                            R_B += T[i, j] * torch.mm(a.view((len(a), 1)), b.view((1, len(b))))
+            # logger.info(Ts)
+            loss /= tdn
+            loss += beta * torch.trace(torch.mm(L.t(), L))
+            logger.info("iteration: %d, loss: %.10f", epoch, loss)
+            if epoch > 0:
+                print((loss-loss_old).item(), options.err_limit)
+            if abs(loss - loss_old) < options.err_limit:
+                logger.info(T)
+                break
             loss_old = loss
 
-        R_I = R_A + options.lambda0*N*torch.eye(dim)
-        #L = inv(R_I) * R_B
-        L, _ = torch.solve(R_B,R_I)
-    # logger.info(time.time()-tic)
-    # for c in range(classnum):
-        # for n in range(trainsetnum[c]):
-            # logger.info(dataset.sentences[c])
-            # logger.info(dataset.orders[c][n])
-            # logger.info(Ts[c][n])
+            R_I = R_A + beta*tdn*torch.eye(dim, dtype=torch.float64, device=device)
+            #L = inv(R_I) * R_B
+            if options.solve_method == 'analytic':
+                R_I = R_I.cpu().numpy()
+                R_B = R_B.cpu().numpy()
+                L = linalg.solve(R_I, R_B)
+                L = torch.from_numpy(L).to(device)
+            if options.solve_method == 'gradient':
+                grad = (R_B - torch.mm(R_I, L))/tdn
+                L += options.lr * grad / max(torch.norm(grad), 1)
+            L += alpha * (L - L.mm(L.transpose(0, 1).mm(L)))
+
+    elif options.solve_method in ['SGD', 'Adagrad', 'RMSprop', 'Adadelta', 'Adam', 'AdamW']:
+        L.requires_grad = True
+        if options.solve_method == 'SGD':
+            op = optim.SGD([L], lr=options.lr, momentum=0.1)
+        elif options.solve_method == 'Adagrad':
+            op = optim.Adagrad([L], lr=options.lr)
+        elif options.solve_method == 'RMSprop':
+            op = optim.RMSprop([L], lr=options.lr)
+        elif options.solve_method == 'Adadelta':
+            op = optim.Adadelta([L], lr=options.lr)
+        elif options.solve_method == 'Adam':
+            op = optim.Adam([L], lr=options.lr)
+        elif options.solve_method == 'AdamW':
+            op = optim.AdamW([L], lr=options.lr)
+
+        for epoch in range(options.max_epoch):
+            loss = 0
+            Vs, Ts = [], []
+            for c in range(classnum):
+                for n in range(tn[c]):
+                    dist, T = get_alignment(torch.mm(trainset[c][n], L), virtual_sequence[c], options)
+                    loss += dist
+                    Vs.append(virtual_sequence[c])
+                    Ts.append(T)
+
+            # logger.info(Ts)
+            loss /= tdn
+            loss += beta * torch.trace(torch.mm(L.t(), L))
+            logger.info("iteration: %d, loss: %.10f", epoch, loss)
+            if epoch > 0:
+                print((loss-loss_old).item(), options.err_limit)
+            if abs(loss - loss_old) < options.err_limit:
+                logger.info(T)
+                break
+            loss_old = loss
+
+            dataset2 = Dataset2(dataset.trainsetdata, Vs, Ts, L, device)
+            dataloader = DataLoader(dataset2, batch_size=64, shuffle=True)
+            running_loss = 0
+            for dist in dataloader:
+                loss = torch.mean(dist) + beta * torch.trace(torch.mm(L.t(), L))
+                op.zero_grad()
+                loss.backward(retain_graph=True)
+                op.step()
+                running_loss += loss.item()
+            print(running_loss)
+
     dataset.L = L
     dataset.virtual = virtual_sequence
     return dataset
